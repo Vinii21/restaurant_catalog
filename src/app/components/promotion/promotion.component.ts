79411802b57d7b9f115d23d506781535637e8c53
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-07-03 16:54:13 -0600

    Ruta de información maquetada y estilada para Mobile

--- src/app/components/promotion/promotion.component.ts
+++ src/app/components/promotion/promotion.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-promotion',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./promotion.component.sass']
 })
 export class PromotionComponent {
-
+  @Input() svgColors: string = "#ffffff"
 }
--- src/app/views/credits/credits.component.ts
+++ src/app/views/credits/credits.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./credits.component.sass']
 })
 export class CreditsComponent {
-
+  svgColors: string = "#000000"
 }
